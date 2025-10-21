612038ec286f6f8e7fb6ff218b21102e4c407fd9
Joinsider <public@joinside.de>
2025-10-21 15:13:02 +0200

    Add unit tests for DateTimeConverter and improve date formatting
    Signed-off-by: Joinsider <public@joinside.de>

--- composeApp/src/commonMain/kotlin/de/joinside/dhbw/data/database/converters/DateTimeConverter.kt
+++ composeApp/src/commonMain/kotlin/de/joinside/dhbw/data/database/converters/DateTimeConverter.kt
@@ -1,17 +1,25 @@
 package de.joinside.dhbw.data.database.converters
 
 import androidx.room.TypeConverter
-import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.number
 
 class DateTimeConverter {
     @TypeConverter
     fun toLocalDate(dateString: String?): LocalDateTime? {
-        return dateString?.let { LocalDateTime.Companion.parse(it) }
+        return dateString?.let { LocalDateTime.parse(it) }
     }
 
     @TypeConverter
     fun fromLocalDate(date: LocalDateTime?): String? {
-        return date?.toString()
+        return date?.let {
+            // Format to ISO-8601 with full precision: yyyy-MM-ddTHH:mm:ss
+            "${it.year.toString().padStart(4, '0')}-" +
+                    "${it.month.number.toString().padStart(2, '0')}-" +
+                    "${it.day.toString().padStart(2, '0')}T" +
+                    "${it.hour.toString().padStart(2, '0')}:" +
+                    "${it.minute.toString().padStart(2, '0')}:" +
+                    it.second.toString().padStart(2, '0')
+        }
     }
 }
